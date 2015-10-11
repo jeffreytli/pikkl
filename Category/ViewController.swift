3d884d7476c056bb67a02e2f851627020cde9e0c
Jeffrey Li <lijeffreytli@gmail.com>
2015-10-11 14:34:07 -0500

    back to where we were last time

--- Category/ViewController.swift
+++ Category/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import Parse
+import ParseFacebookUtilsV4
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnFBLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +28,26 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func loginFB(sender: AnyObject) {
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(["public_profile", "email"], block: { (user:PFUser?, error:NSError?) -> Void in
+            if(error != nil) {
+                //TODO display an alert message (from youtube vid)
+                print("ERROR!!")
+            }
+            print(user)
+            print("Current user token = \(FBSDKAccessToken.currentAccessToken().tokenString)")
+            print("Current user ID = \(FBSDKAccessToken.currentAccessToken().userID)")
+            
+//            if(FBSDKAccessToken.currentAccessToken() != nil) {
+//                let protectedPage = self.storyboard?.instantiateViewControllerWithIdentifier("BattleTVC") as! BattleTableViewController
+//                
+//                let protectedPageNav = UINavigationController(rootViewController: protectedPage)
+//                
+//                let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+//                
+//                appDelegate.window?.rootViewController = protectedPage
+//            }
+        })
+    }
 }
 
