352a62c7cd8b1a04cfde5251258c30c0742d8f04
Jeffrey Li <lijeffreytli@gmail.com>
2015-10-26 00:27:34 -0500

    fixed a bug where the transition wasn't working

--- Category/CreateBattleViewController.swift
+++ Category/CreateBattleViewController.swift
@@ -97,7 +97,10 @@ class CreateBattleViewController: UIViewController {
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         
-        appDelegate.window?.rootViewController?.presentViewController(controllerNav, animated: true, completion: nil)
+        UIView.transitionWithView(appDelegate.window!, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+            appDelegate.window?.rootViewController = controllerNav
+            }, completion: nil)
         
+//        appDelegate.window?.rootViewController = controllerNav
     }
 }
