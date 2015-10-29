47a1b1037bc321393ef7cc49bc67b7329dbf8403
Morgan Edgerton <medgerton@rmn.com>
2015-10-28 20:46:32 -0500

    Can upload image from gallery to UI

--- Category/SubmitViewController.swift
+++ Category/SubmitViewController.swift
@@ -8,20 +8,26 @@
 
 import UIKit
 
-class SubmitViewController: UIViewController {
+class SubmitViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var lblBattleTitle: UILabel!
     
     @IBOutlet weak var imgSubmit: UIImageView!
     
+    let imagePicker = UIImagePickerController()
     
     @IBAction func imgTapSubmit(sender: AnyObject) {
         //open camera for image upload
-        lblBattleTitle.text! = "tap !!"
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         imgSubmit.image = UIImage(named: "cameraIcon")
+        
         // Do any additional setup after loading the view.
     }
 
@@ -29,16 +35,21 @@ class SubmitViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - UIImagePickerControllerDelegate Methods
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imgSubmit.contentMode = .ScaleAspectFill
+            imgSubmit.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
