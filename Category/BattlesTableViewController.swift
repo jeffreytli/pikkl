bcf95990a2ee8fb74a2d360ad5643fd2f0209546
Jeffrey Li <lijeffreytli@gmail.com>
2015-10-30 22:10:37 -0500

    cleaned up code for battlestableviewcontroller

--- Category/BattlesTableViewController.swift
+++ Category/BattlesTableViewController.swift
@@ -15,11 +15,8 @@ class BattlesTableViewController: UITableViewController {
 
     let textCellIdentifier = "BattleCell"
     
-    var battleIDs:[String] = []
-
-    let battleCategories = ["Ugliest Friends", "Drunk Moments", "Worst Gift Ever", "My Super Powers", "Why Im Single", "Embarrsssinggggg", "Lemme Take A Selfie", "Worst Roommate Award", "Worst Picture Ever", "Im Stupid", "Best Caption", "Blessed", "I Hate Life"]
-    
-//    @IBOutlet weak var battlesTableView: UITableView!
+    var battleIds:[String] = []
+    var battleNames:[String] = []
     
     @IBOutlet weak var battlesTableView: UITableView!
     
@@ -39,20 +36,16 @@ class BattlesTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return battleCategories.count
-        return battleIDs.count
+        return battleIds.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
         
         let cell: BattleTableViewCell = tableView.dequeueReusableCellWithIdentifier("BattleCell") as! BattleTableViewCell
         
         let row = indexPath.row
-//        cell.lblBattleName.text = battleCategories[row];
-//        cell.textLabel?.text = battleCategories[row];
         
-        cell.lblBattleName.text = battleIDs[row];
+        cell.lblBattleName.text = battleNames[row];
         
         return cell
     }
@@ -81,10 +74,8 @@ class BattlesTableViewController: UITableViewController {
     
     func fetchAllObjects() {
         let query: PFQuery = PFQuery(className: "Battle")
-        // query.whereKey("name", equalTo: PFUser.currentUser()!.username!)
         
         query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
-            
             if error == nil {
                 // The find succeeded.
                 print("Successfully retrieved \(objects!.count)  jobs from database.")
@@ -94,13 +85,14 @@ class BattlesTableViewController: UITableViewController {
                     for object in objects {
                         print(object.objectId)
                         print(object["name"])
-                        self.battleIDs.append(object.objectId! as String)
+                        self.battleIds.append(object.objectId! as String)
+                        self.battleNames.append(object["name"] as! String)
                         self.tableView.reloadData()
                     }
                 }
             } else {
-                // Log details of the failure
-                // println("Error: \(error!) \(error!.userInfo!)")
+//                 Log details of the failure
+                 print("Error: \(error!)")
             }
         }
     }
