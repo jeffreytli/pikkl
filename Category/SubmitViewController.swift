a6c5f6ebe4f3830af22767ccc37309477d1c6bc4
Jeffrey Li <lijeffreytli@gmail.com>
2015-11-22 16:35:53 -0600

    adding logic for submit view

--- Category/SubmitViewController.swift
+++ Category/SubmitViewController.swift
@@ -16,7 +16,7 @@ import CoreData
 class SubmitViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
    // @IBOutlet weak var lblBattleTitle: UILabel!
-    @IBOutlet weak var imgSubmit: UIImageView!
+    var imgSubmit: UIImageView!
     @IBOutlet weak var lblBattleTitle: UILabel!
     @IBOutlet weak var btnSubmit: UIButton!
     
@@ -28,9 +28,7 @@ class SubmitViewController: UIViewController, UIImagePickerControllerDelegate, U
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
-        imgSubmit.image = UIImage(named: "SubmitCamera")
         lblBattleTitle.text = battleTitle
-
     }
     
     override func shouldAutorotate() -> Bool {
@@ -53,16 +51,30 @@ class SubmitViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imgSubmit.contentMode = .ScaleAspectFill
-            imgSubmit.image = pickedImage
+//            imgSubmit.image = pickedImage
             imgUploaded = true;
         }
         dismissViewControllerAnimated(true, completion: nil)
-
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func btnCameraTapped(sender: UIButton) {
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnGalleryTapped(sender: AnyObject) {
+        //open camera for image upload
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
     
     @IBAction func imgTapSubmit(sender: AnyObject) {
         //open camera for image upload
