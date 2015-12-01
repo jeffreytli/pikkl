da9e544eb63bf458f2b974042c711f6651a1e15f
Morgan Edgerton <medgerton@rmn.com>
2015-12-01 01:23:42 -0600

    Create color and fonts

--- Category/CreateBattleViewController.swift
+++ Category/CreateBattleViewController.swift
@@ -43,8 +43,6 @@ class CreateBattleViewController: UIViewController, UITextFieldDelegate {
     var friendNames = [String]()
     var battlePhaseLength = 30
     
-    let createBlue = UIColor(red: 14, green: 223, blue: 124, alpha: 1)
-    
     var battleTitles = ["Cool Socks", "Architecture", "Patterns", "Fail", "#Selfie", "Shadows", "Embarrassing", "Cute Dogs", "Cute Cats", "#Flawless", "2015", "Nature", "Drunk Face", "Duck Face", "Ugliest Person", "Food", "Stupid Animals", "Campus Life", "FML", "#Drunk", "Alcohol", "LOL", "Worst Friends", "Basic", "Sexy","#MomTexts", "#WorstGift", "Weirdo", "#Gross", "#Single", "#GymProbs", "#Fabulous", "Beautiful", "#TrueLove", "Love Sucks", "#BFProbs", "GFProbs", "Screwed", "Bad Luck", "Best Shit", "Ugly Cat", "Ugly Dog", "#Bored", "Scenic", "Abstract", "Urban Art", "Street", "Flower", "Portrait", "Landscape", "Deep", "Inception","Children", "Urban", "Rural", "Fashion", "Travel", "Action", "Night", "Night Life", "Dawn", "Dusk", "Insect", "Cars", "Family", "Pets", "Funny", "Wedding", "Mature", "Texture", "Vintage", "Concert", "Music", "Classic", "Frat Life", "Cute", "Shredding", "Happy", "#swag", "Belieber", "ThrowBack", "Light", "Fire"]
     var chosenTitles = [Int]()
     
@@ -54,6 +52,7 @@ class CreateBattleViewController: UIViewController, UITextFieldDelegate {
         
         configureNavView()
         setAllButtonDetails()
+
     }
     
     override func shouldAutorotate() -> Bool {
@@ -74,17 +73,16 @@ class CreateBattleViewController: UIViewController, UITextFieldDelegate {
     func configureNavView() {
         
         // Change the font and size of nav bar text
-        if let navBarFont = UIFont(name: "Helvetica", size: 20.0) {
-            print("has font")
+        if let navBarFont = UIFont(name: "GothamBlack", size: 20.0) {
             let navBarAttributesDictionary: [String: AnyObject]? = [
-                NSForegroundColorAttributeName: createBlue,
+                NSForegroundColorAttributeName: UIColor.whiteColor(),
                 NSFontAttributeName: navBarFont
             ]
             navigationController?.navigationBar.titleTextAttributes = navBarAttributesDictionary
         }
-        navigationController?.navigationBar.barTintColor = UIColor.blackColor()
-
-        
+        let createBlue = UIColor(red:0/255, green:102/255, blue:255/255, alpha:1.0)
+        navigationController?.navigationBar.barTintColor = createBlue as UIColor
+        print(createBlue)
     }
 
     
